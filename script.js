e7280fe8a88fa543aa4fe60667bfe28d5392727e
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-26 22:11:33 +0530

    added slider

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+ 
+ var angle = 0;
+ var slider;
+ 
  function setup() {
     createCanvas(1200, 1200);
+    slider = createSlider(0, TWO_PI, PI/4, 0.01);
  }
 
  function draw() {
     background(0);
-
+    angle = slider.value();
     stroke(0, 255, 0);
     translate(600, height);
-    branch(400);
+    branch(300);
 
     
  }
@@ -15,6 +20,9 @@
  function branch(len) {
     line(0, 0, 0, -len);
     translate(0, -len);
-    rotate(PI / 4);
-    line(0, 0, 0, -len * 0.67); 
+    rotate(angle);
+    if(len > 4) {
+        branch(len * 0.67);
+    }
+    // line(0, 0, 0, -len * 0.67); 
 }
